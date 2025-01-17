2325371390c99ccda1714958ba81bebb04bb6032
Havish Gadey <168265984+havish7728@users.noreply.github.com>
2025-01-17 15:39:27 +0530

    Update settings.py

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
-    '.ngrok-free.app',  # Add your Ngrok domain here
+    '.ngrok-free.app',
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
